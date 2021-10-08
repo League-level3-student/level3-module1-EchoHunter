de3fafdd31e7ee1a3b051fc67188f51459d3410e
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2021-10-07 18:31:08 -0700

    Add files via upload

--- src/_07_California_Weather/CaliforniaWeather.java
+++ src/_07_California_Weather/CaliforniaWeather.java
@@ -1,6 +1,13 @@
 package _07_California_Weather;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /*
  * OBJECTIVE:
@@ -9,7 +16,7 @@ import java.util.HashMap;
  * and the Utilities class inside this project to get the temperature data
  * from a day in December 2020.
  * Example: User: Encinitas
- *          Program: Encinitas is Overcast with a tempeature of 59.01 °F
+ *          Program: Encinitas is Overcast with a tempeature of 59.01 ï¿½F
  * 
  * 2. Create a way for the user to specify the weather condition and then
  * list the cities that have those conditions.
@@ -18,7 +25,7 @@ import java.util.HashMap;
  * 
  * 3. Create a way for the user to enter a minimum and maximum temperature
  * and then list the cities that have temperatures within that range
- * Example: User: minimum temperature °F = 65.0, max temperature °F = 70.0
+ * Example: User: minimum temperature ï¿½F = 65.0, max temperature ï¿½F = 70.0
  *          Program: Fortana, Glendale, Escondido, Del Mar, ...
  *          
  * EXTRA:
@@ -27,19 +34,53 @@ import java.util.HashMap;
  * temperature, you can get a free API key at: https://openweathermap.org/api 
  */
 
-public class CaliforniaWeather {
+public class CaliforniaWeather implements ActionListener {
+    JButton getCityWeather = new JButton();
+    JButton getConditionList = new JButton();
+    JButton getMaxAndMinTemp = new JButton();
+    JPanel panel = new JPanel();
+    JFrame frame = new JFrame();
     
+    HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
+    String cityName = "";
+    WeatherData datum = weatherData.get(cityName);
     void start() {
-        HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
-        
+        getCityWeather.addActionListener(this);
+        getConditionList.addActionListener(this);
+        getMaxAndMinTemp.addActionListener(this);
+        panel.add(getCityWeather);
+        panel.add(getConditionList);
+        panel.add(getMaxAndMinTemp);
+        frame.add(panel);
+        frame.setSize(500,250);
+        frame.setVisible(true);
         // All city keys have the first letter capitalized of each word
-        String cityName = Utilities.capitalizeWords( "National City" );
-        WeatherData datum = weatherData.get(cityName);
         
-        if( datum == null ) {
-            System.out.println("Unable to find weather data for: " + cityName);
-        } else {
-            System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
-        }
+       
+        
+        
     }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == getCityWeather) { 
+			cityName = JOptionPane.showInputDialog("Which city would you like to check? (the first letter of every word should be capitalized)");
+			datum = weatherData.get(cityName);
+			if( datum == null ) {
+	            JOptionPane.showMessageDialog(null,"Unable to find weather data for: " + cityName);
+	        } else {
+	            JOptionPane.showMessageDialog(null,cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+	        }
+	}
+		if(e.getSource() == getConditionList) {
+			String condition = JOptionPane.showInputDialog("What Conditions are you looking for? (the first letter of every word should be capitalized)");
+			for (String w: weatherData.keySet()) {
+				
+			}
+			
+			}
+		if(e.getSource() == getMaxAndMinTemp) {
+			
+		}
+	}
 }
